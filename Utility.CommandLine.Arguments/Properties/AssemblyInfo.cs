2a147d6f14707cbf266b067fe7fd015716ed1fba
JP Dillingham <jpdillingham@gmail.com>
2017-03-27 19:16:46 -0500

    Increased version to 1.0.1

--- Utility.CommandLine.Arguments/Properties/AssemblyInfo.cs
+++ Utility.CommandLine.Arguments/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b739dd22-4bd3-4d17-a351-33d129e9fe30")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]

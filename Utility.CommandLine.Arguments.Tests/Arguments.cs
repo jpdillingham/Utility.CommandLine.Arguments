59f747d73357d7bfe165225a52bd641bfaa24944
JP Dillingham <jpdillingham@gmail.com>
2017-04-14 07:12:06 -0500

    Added tests to cover null reference exceptions when Populate() is used on a class with no properties.

--- Utility.CommandLine.Arguments.Tests/Arguments.cs
+++ Utility.CommandLine.Arguments.Tests/Arguments.cs
@@ -539,4 +539,42 @@ namespace Utility.CommandLine.Tests
 
         #endregion Public Methods
     }
+
+    /// <summary>
+    ///     Unit tests for the <see cref="CommandLine.Arguments"/> class.
+    /// </summary>
+    /// <remarks>Used to facilitate testing of a class with no properties.</remarks>
+    [Collection("Arguments")]
+    public class TestClassWithNoProperties
+    {
+        #region Public Methods
+
+        /// <summary>
+        ///     Tests the <see cref="Utility.CommandLine.Arguments.Populate(Type, string)"/> method with an explicit string
+        ///     containing a single argument/value pair, and with a Type containing no properties.
+        /// </summary>
+        [Fact]
+        public void PopulateArguments()
+        {
+            Exception ex = Record.Exception(() => CommandLine.Arguments.Populate(GetType(), "--hello world"));
+
+            Assert.NotNull(ex);
+            Assert.IsType<InvalidCastException>(ex);
+        }
+
+        /// <summary>
+        ///     Tests the <see cref="Utility.CommandLine.Arguments.Populate(Type, string)"/> method with an explicit string
+        ///     containing two operands, and with a Type containing no properties.
+        /// </summary>
+        [Fact]
+        public void PopulateOperands()
+        {
+            Exception ex = Record.Exception(() => CommandLine.Arguments.Populate(GetType(), "one two"));
+
+            Assert.NotNull(ex);
+            Assert.IsType<InvalidCastException>(ex);
+        }
+
+        #endregion Public Methods
+    }
 }

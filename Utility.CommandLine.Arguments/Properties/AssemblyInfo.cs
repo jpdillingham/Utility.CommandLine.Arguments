3d089c0a1a47d1778101605ad0dfc638b505b340
JP Dillingham <jpdillingham@gmail.com>
2018-11-24 11:01:35 -0600

    bump version to 1.3.4

--- Utility.CommandLine.Arguments/Properties/AssemblyInfo.cs
+++ Utility.CommandLine.Arguments/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b739dd22-4bd3-4d17-a351-33d129e9fe30")]
-[assembly: AssemblyVersion("1.3.3")]
-[assembly: AssemblyFileVersion("1.3.3")]
+[assembly: AssemblyVersion("1.3.4")]
+[assembly: AssemblyFileVersion("1.3.4")]

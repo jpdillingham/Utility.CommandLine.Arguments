bcf054d5cba533a19a90dc5217ad077243c3c952
Byron Watts <btwatts@gmail.com>
2018-11-26 10:03:20 -0600

    Added extra Boolean processing to handle :yes :y :true :t :no :n :false :f processing.

--- Utility.CommandLine.Arguments/Arguments.cs
+++ Utility.CommandLine.Arguments/Arguments.cs
@@ -50,6 +50,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
@@ -452,7 +453,30 @@ namespace Utility.CommandLine
         {
             try
             {
-                return Convert.ChangeType(value, toType);
+                if (toType == typeof(System.Boolean))
+                {
+                    string myValue = value.ToString();
+                    if ((myValue.Length == 0)
+                    || (string.Equals(myValue, "yes", StringComparison.OrdinalIgnoreCase))
+                    || (string.Equals(myValue, "y", StringComparison.OrdinalIgnoreCase))
+                    || (string.Equals(myValue, "true", StringComparison.OrdinalIgnoreCase))
+                    || (string.Equals(myValue, "t", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        return true;
+                    }
+                    else
+                    if ((string.Equals(myValue, "no", StringComparison.OrdinalIgnoreCase))
+                    || (string.Equals(myValue, "n", StringComparison.OrdinalIgnoreCase))
+                    || (string.Equals(myValue, "false", StringComparison.OrdinalIgnoreCase))
+                    || (string.Equals(myValue, "f", StringComparison.OrdinalIgnoreCase)))
+                    {
+                        return false;
+                    }
+
+                    Boolean.TryParse(myValue, out bool myVal);
+                    return Convert.ToBoolean(value);
+                }
+                return Convert.ChangeType(value, toType, CultureInfo.InvariantCulture);
             }
             catch (Exception ex)
             {

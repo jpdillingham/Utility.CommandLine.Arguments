b20bb54b06343336c1135946a6b3e784d9cdae66
JP Dillingham <jpdillingham@gmail.com>
2019-03-16 09:09:49 -0500

    remove errant analysis suppression

--- tests/Utility.CommandLine.Arguments.Tests/ArgumentsTests.cs
+++ tests/Utility.CommandLine.Arguments.Tests/ArgumentsTests.cs
@@ -30,11 +30,8 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Xunit;
-
-[assembly: SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:FileMayOnlyContainASingleClass", Justification = "Reviewed.")]
 
 namespace Utility.CommandLine.Tests
 {

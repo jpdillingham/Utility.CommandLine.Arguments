cc36f867bd662c7619a7523e482c67bfb967c140
JP Dillingham <jpdillingham@gmail.com>
2019-04-07 15:17:20 -0500

    add branch to sonar config

--- build/ci.sh
+++ build/ci.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__branch=$(git branch --no-color | grep -E '^\*' | awk '{print $2}')
 
-dotnet-sonarscanner begin /key:"jpdillingham_Utility.CommandLine.Arguments" /o:jpdillingham-github /d:sonar.host.url="https://sonarcloud.io" /d:sonar.exclusions="**/*examples*/**" /d:sonar.login="${TOKEN_SONARCLOUD}" /d:sonar.cs.opencover.reportsPaths="tests/opencover.xml"
+dotnet-sonarscanner begin /key:"jpdillingham_Utility.CommandLine.Arguments" /o:jpdillingham-github /d:sonar.host.url="https://sonarcloud.io" /d:sonar.exclusions="**/*examples*/**" /d:sonar.branch.name=${__branch} /d:sonar.login="${TOKEN_SONARCLOUD}" /d:sonar.cs.opencover.reportsPaths="tests/opencover.xml"
 
 . "${__dir}/build.sh"
 . "${__dir}/test.sh"
